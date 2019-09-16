6f158b93159854cc6a3ff97ac24172dda2a14bb9
Jonas Hals <contact@jonashals.me>
2019-09-16 19:32:38 +0200

    Finish work to find coin name from tickerˆ

--- index.js
+++ index.js
@@ -1,56 +1,62 @@
 const request = require('request');
 const _ = require('lodash');
 
-const convertFromTicker = (ticker) => {
+const convertFromTicker = (ticker, callback) => {
   request({
     url: "https://api.coingecko.com/api/v3/coins/list",
     json: true
   }, (error, response, body) => {
     if (error || response.statusCode >= 400) {
-      return ticker;
+        return callback("");
     } else {
-      const result = _.filter(body, x => x.symbol === ticker.toLowerCase());
-      console.log(result[0]);
-      return result[0].id;
+        let coin = body.find(x => x.symbol.toLowerCase() === ticker.toLowerCase());
+        if (typeof coin === "undefined")
+            return callback("");
+        return callback(coin.name.toLowerCase());
     }
   });
-}
+};
 
 const createRequest = (input, callback) => {
-  let url = "https://api.coingecko.com/api/v3/simple/price";
-  let coin = input.data.coin || "ethereum";
-  coin = convertFromTicker(coin);
-  const market = input.data.market || "USD";
-  url = url + coin;
+    convertFromTicker(input.data.coin || "ETH", (coin) => {
+        let url = "https://api.coingecko.com/api/v3/simple/price";
+        const market = input.data.market || "usd";
 
-  const queryObj = {
-    ids: coin,
-    vs_currencies: market
-  };
+        const queryObj = {
+            ids: coin,
+            vs_currencies: market
+        };
 
-  const options = {
-    url: url,
-    qs: queryObj,
-    json: true
-  };
-  request(options, (error, response, body) => {
-    if (error || response.statusCode >= 400) {
-      callback(response.statusCode, {
-        jobRunID: input.id,
-        status: "errored",
-        error: body,
-        errorMessage : body.error,
-        statusCode: response.statusCode
-      });
-    } else {
-      callback(response.statusCode, {
-        jobRunID: input.id,
-        data: body,
-        result: body.market_data.current_price.usd,
-        statusCode: response.statusCode
-      });
-    }
-  });
+        const options = {
+            url: url,
+            qs: queryObj,
+            json: true
+        };
+        request(options, (error, response, body) => {
+            if (_.isEmpty(body)) {
+                response.statusCode = 403; // Force status code if response is empty
+                body = {
+                    "error": "Empty response"
+                }
+            }
+            if (error || response.statusCode >= 400) {
+                callback(response.statusCode, {
+                    jobRunID: input.id,
+                    status: "errored",
+                    error: body,
+                    errorMessage: body.error,
+                    statusCode: response.statusCode
+                });
+            } else {
+                callback(response.statusCode, {
+                    jobRunID: input.id,
+                    data: body,
+                    result: body[coin][market.toLowerCase()],
+                    statusCode: response.statusCode
+                });
+            }
+        });
+    })
 };
 
 // This is a wrapper to allow the function to work with
@@ -67,7 +73,7 @@ exports.handler = (event, context, callback) => {
   createRequest(event, (statusCode, data) => {
     callback(null, data);
   });
-}
+};
 
 // This allows the function to be exported for testing
 // or for running in express
